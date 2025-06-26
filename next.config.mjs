143d552af71d5b93b96f09eddd5ff5ea8fb706e2
KentaMiyazaki-09 <k.miyazaki@rhino-inc.jp>
2025-06-26 16:34:58 +0900

    next.config.mjsを修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,40 @@
 /** @type {import('next').NextConfig} */
 
-import path from "path"
+import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// /opt/build/repo/node_modules/stretchで
+// fsモジュールを参照しているが、クライアントサイドには存在しないためNetlify上でbuildが失敗する
+// fsモジュールを空扱いする
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
 
 const nextConfig = {
-  images: { // microcmsから取得した画像の表示を許可
+  images: {
+    // microcmsから取得した画像の表示を許可
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io'
-      }
-    ]
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
   },
-  sassOptions: { // mixin.scssグローバルで読み込めるように設定
-    includePaths: [path.join(__dirname, 'src/styles')],
-  }
-
+  sassOptions: {
+    // mixin.scssグローバルで読み込めるように設定
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
 };
 
 export default nextConfig;
