a42b1020dd280be8b6c4ebbdca5c237632af798f
Riho yamamoto <r.yamamoto@rhino-inc.jp>
2025-08-07 16:58:22 +0900

    header修正

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/navigation"; // ← 追加
 
 const Header = () => {
   return (
-    <header className="w-full fixed top-0 left-0 z-[210] px-common-sp md:px-common-pc pt-[28px] pb-[50px] md:pt-[70px] md:pb-[90px] md:z-[500]">
+    <header className="w-full fixed top-0 left-0 z-[210] px-common-sp md:px-common-pc pt-[28px] pb-[50px] md:pt-[70px] md:pb-[0px] md:z-[500]">
       <div>
         <h1 className="relative w-[240px] h-[65px] md:w-[360px] md:h-[100px]">
           <a href="/">
